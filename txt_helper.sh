db93364fa5bd801263de16f34935c39048409c4d
Artixisthebest <84476835+Artixisthebest@users.noreply.github.com>
2024-02-15 13:12:58 -0800

    Update txt_helper.sh
    Added misc code

--- txt_helper.sh
+++ txt_helper.sh
@@ -1,7 +1,7 @@
 
-links=( "https://zoom.us/client/5.17.5.29101/zoomusInstallerFull.pkg" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/VSCode-darwin-universal.zip" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "https://downloads.arduino.cc/arduino-ide/arduino-ide_latest_macOS_64bit.dmg" "https://redirector.gvt1.com/edgedl/android/studio/install/2023.1.1.28/android-studio-2023.1.1.28-mac.dmg")
-arm_links=( "https://zoom.us/client/5.17.5.29101/zoomusInstallerFull.pkg" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/VSCode-darwin-universal.zip" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "https://downloads.arduino.cc/arduino-ide/arduino-ide_latest_macOS_64bit.dmg" "https://redirector.gvt1.com/edgedl/android/studio/install/2023.1.1.28/android-studio-2023.1.1.28-mac_arm.dmg")
-packages=( "zoomusInstallerFull.pkg" "VSCode-darwin-universal.zip" "" "" ""  )
+links=( "https://zoom.us/client/5.17.5.29101/zoomusInstallerFull.pkg" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/VSCode-darwin-universal.zip" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "https://downloads.arduino.cc/arduino-ide/arduino-ide_2.3.1_macOS_64bit.dmg" "https://redirector.gvt1.com/edgedl/android/studio/install/2023.1.1.28/android-studio-2023.1.1.28-mac.dmg" "https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=mac" "https://dl.pstmn.io/download/latest/osx_64" "https://studio3t.com/download-thank-you/?OS=osx" "https://repo.anaconda.com/archive/Anaconda3-2023.09-0-MacOSX-x86_64.pkg")
+arm_links=( "https://zoom.us/client/5.17.5.29101/zoomusInstallerFull.pkg" "https://vscode.download.prss.microsoft.com/dbazure/download/stable/31c37ee8f63491495ac49e43b8544550fbae4533/VSCode-darwin-universal.zip" "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg" "https://downloads.arduino.cc/arduino-ide/arduino-ide_2.3.1_macOS_arm64.dmg" "https://redirector.gvt1.com/edgedl/android/studio/install/2023.1.1.28/android-studio-2023.1.1.28-mac_arm.dmg" "https://www.jetbrains.com/pycharm/download/download-thanks.html?platform=macM1" "https://dl.pstmn.io/download/latest/osx_arm64" "https://studio3t.com/download-thank-you/?OS=osx_aarch64" "https://repo.anaconda.com/archive/Anaconda3-2023.09-0-MacOSX-arm64.pkg")
+packages=( "zoomusInstallerFull.pkg" "VSCode-darwin-universal.zip" "googlechrome.dmg" "arduino-ide.dmg" "android-studio.dmg" "pycharm.dmg" "postman.zip" "Studio-3T.dmg" "anaconda.pkg")
 
 EXTENSION_DIR="$HOME/.vscode/extensions"
 # Create the extension directory if it doesn't exist
@@ -17,8 +17,10 @@ xcode-select --install
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 
 # install node version manager
-brew install nvm
+brew install nvm 
 
+# install git
+brew install git
 
 nvm install 14.20.0
 
@@ -32,9 +34,9 @@ nvm install node
 nvm alias default v16.14.2
 
 # install yarn
-npm i -g yarn
+npm i -g yarn truffle
 
-## the following code is to remove the python bloat
+# the following code is to remove the python bloat
 cd /Library
 
 # removing python
@@ -54,17 +56,18 @@ sudo rm -rf /usr/local/bin/python
 #wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-MacOSX-x86_64.pkg // get arm link
 
 
-
 brew install wget
 
 function unzip_package() {
     local i=$1
-    wget "${links[$i]}"
+    # -O so that the output is always the same 
+    wget "${links[$i]}" -O "${packages[$i]}"
     unzip "${packages[$i]}" -d /Applications
 }
 
 function damage_installer() {
-    wget "${links[$i]}"
+    local i=$1
+    wget "${links[$i]}" -O "${packages[$i]}"
 } 
 
 function package_installer() {
@@ -73,7 +76,7 @@ function package_installer() {
     if [[ "$(uname -m)" != "x86_64" ]]; then
         link="${arm_links[$i]}"
     fi
-    wget "${link}"
+    wget "${link}" -O "${packages[$i]}"
     sudo installer -pkg "${packages[$i]}" -target ../
 }
 
@@ -117,6 +120,6 @@ mv ~/.vscode/extensions/* "$EXTENSION_DIR"
 echo "Setup complete!"
 
 rm -rf *.pkg
-
 rm -rf *.zip
+
 echo "Finished Installing"
